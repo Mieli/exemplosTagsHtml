c66009c2e1be31e3ab68c0b0fed49271407b901b
MIELI <alessandro.mieli@gmail.com>
2020-05-09 21:04:08 -0300

    estudado a tag span e form

--- servidor.js
+++ servidor.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
+app.get('/convidado', (req, res) => {
+    const nome = req.query.guessName;
+    res.send("Você indormou o nome "+nome+" enviado via GET");
+});
+
+app.post('/convidado', (req, res) => {
+    const nome = req.body.guessName;
+    res.send("Você indormou o nome "+nome+ " Enviado via POST");
+});
 
 app.listen(PORT, () => console.log("Servidor rodando no endereço: http://localhost:"+PORT));
